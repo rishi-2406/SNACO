d2f16c11b36b3550d09ac42683713f6805712eb2
Chaitanya <chaitnayabhagat666@gmail.com>
2025-10-19 12:06:51 +0530

    basic map integration
    just a map without database

---
+++ backend/server/server.js
@@ -0,0 +1,56 @@
+
+const express = require('express');
+const app = express();
+const { SuperfaceClient } = require('@superfaceai/one-sdk');
+const sdk = new SuperfaceClient();
+const PORT = 5000;
+app.use(express.json());
+
+async function geocodeLocation(loc) {
+  // Load the profile
+  const profile = await sdk.getProfile('address/geocoding@3.1.2');
+
+  // Use the profile
+  const result = await profile.getUseCase('Geocode').perform(
+    {
+      query: loc,
+    },
+    {
+      provider: 'nominatim',
+    }
+  );
+
+  // Handle the result
+  const data = result.unwrap();
+  return data;
+}
+
+app.get('/api/geocode', async (req, res) => {
+  try {
+    const location = req.query.location;
+    const coordinates = await geocodeLocation(location);
+    res.json({ location, coordinates });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.post('/api/route', async (req, res) => {
+  try {
+    const locations = req.body.locations;
+    if (locations.length !== 2) {
+      res.status(422).json({ error: 'Expected 2 waypoints' });
+      return;
+    }
+    const waypoints = await Promise.all(
+      locations.map((location) => geocodeLocation(location))
+    );
+    res.json({ waypoints });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,134 @@
+// src/App.js
+
+import './App.css';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faRoute } from '@fortawesome/free-solid-svg-icons';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet-defaulticon-compatibility';
+
+import RoutingMachine from './RoutingMachine';
 
 function App() {
+  const [locationMarkers, setLocationMarkers] = useState([]);
+  const [waypoints, setWaypoints] = useState();
+  const [showRoutingForm, setFormView] = useState(false);
+
+  useEffect(() => {}, [waypoints]);
+
+  async function handleMarkerSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const inputLocation = formData.get('location');
+
+    const res = await fetch(
+      '/api/geocode?' +
+        new URLSearchParams({ location: inputLocation }).toString()
+    );
+    if (!res.ok) {
+      const err = await res.text();
+      alert(`Something went wrong.\n${err}`);
+    } else {
+      const data = await res.json();
+      let newLocation = {
+        address: data.location,
+        lat: data.coordinates.latitude,
+        long: data.coordinates.longitude,
+      };
+      setLocationMarkers((locations) => [...locations, newLocation]);
+    }
+  }
+
+  async function handleRouteSubmit(event) {
+    event.preventDefault();
+    // Reset previous waypoints
+    if (waypoints) {
+      setWaypoints();
+    }
+    // Hide the form
+    setFormView(false);
+
+    const formData = new FormData(event.target);
+    const locations = formData.getAll('location');
+    const res = await fetch('/api/route', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify({ locations }),
+    });
+    if (!res.ok) {
+      const err = await res.text();
+      alert(`Something went wrong.\n${err}`);
+    } else {
+      const data = await res.json();
+      setWaypoints(data.waypoints);
+    }
+  }
 
   return (
-    <>  
-      <p className="bg-amber-600" >This is snaco</p>
-    </>
-  )
+    <div className="App">
+      <form className="inputBlock" onSubmit={handleMarkerSubmit}>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          required
+          placeholder="Enter location"
+        />
+        <button type="submit" className="addloc">
+          <FontAwesomeIcon icon={faLocationDot} style={{ color: '#1EE2C7' }} />
+        </button>
+      </form>
+      <div className="routeBlock">
+        <div className="addRoutes">
+          {showRoutingForm && (
+            <form onSubmit={handleRouteSubmit}>
+              <div className="posOne">
+                <input
+                  type="text"
+                  name="location"
+                  required
+                  placeholder="Staring Point"
+                />
+              </div>
+              <div className="posTwo">
+                <input
+                  type="text"
+                  name="location"
+                  required
+                  placeholder="End Point"
+                />
+              </div>
+              <button className="addloc">Find Path</button>
+            </form>
+          )}
+          <FontAwesomeIcon
+            icon={faRoute}
+            style={{ color: '#1EE2C7' }}
+            onClick={() => {
+              setFormView((showRoutingForm) => !showRoutingForm);
+            }}
+          />
+        </div>
+      </div>
+      <MapContainer center={[31.505, 70.09]} id="mapId" zoom={4}>
+        {locationMarkers.map((loc, key) => {
+          return (
+            <Marker key={key} position={[loc.lat, loc.long]}>
+              <Popup>{loc.address}</Popup>
+            </Marker>
+          );
+        })}
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {waypoints ? <RoutingMachine waypoints={waypoints} /> : ''}
+      </MapContainer>
+    </div>
+  );
 }
 
-export default App
+export default App;
